8b6c5856f08392c3815c6c7a822228b837686525
TimzonaTimothy <timzonatimothy@gmail.com>
2025-04-01 16:32:40 -0700

    search screen

---
+++ lib/screens/search/widgets/app_ticket_tabs.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTicketTabs extends StatelessWidget {
+  const AppTicketTabs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
